d5df20562bd287c07e6a2b4470639cca81fd599c
seonghoo1217 <seonghoo1217@naver.com>
2023-01-19 00:02:32 +0900

    feat : Server HealthCheck API

---
+++ mogakco/src/main/java/project/mogakco/domain/heathcheck/api/HealthCheckController.java
@@ -0,0 +1,18 @@
+package project.mogakco.domain.heathcheck.api;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+public class HealthCheckController {
+
+	@GetMapping("/api/v1/healthcheck")
+	public ResponseEntity<String> serverHealthCheckAPI(){
+		log.info("ping-pong");
+		return new ResponseEntity<>("PingPong", HttpStatus.OK);
+	}
+}
