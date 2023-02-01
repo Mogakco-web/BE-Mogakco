940b57215cd4fdcf3193698d648f0cddeb5abcb4
seonghoo1217 <seonghoo1217@naver.com>
2023-02-01 23:19:22 +0900

    #1 chore : apply to Secret Manager on the hibernate jdbc

--- mogakco/src/main/java/project/mogakco/MogakcoApplication.java
+++ mogakco/src/main/java/project/mogakco/MogakcoApplication.java
@@ -1,10 +1,20 @@
 package project.mogakco;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class MogakcoApplication {
+	/*@Value("${jdbc-url}")
+	private String test;
+
+	@PostConstruct
+	public void test(){
+		System.out.println(">>>>" +test);
+	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(MogakcoApplication.class, args);
