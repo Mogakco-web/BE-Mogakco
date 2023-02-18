03f05f7e7ac0f1e3b679f36daf789fdbbf5967be
seonghoo1217 <seonghoo1217@naver.com>
2023-02-18 19:55:51 +0900

    #2 feat : add extends WebSecurityConfigureAdapter

--- mogakco/src/main/java/project/mogakco/global/config/SecurityConfig.java
+++ mogakco/src/main/java/project/mogakco/global/config/SecurityConfig.java
@@ -9,7 +9,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -27,7 +29,7 @@ import project.mogakco.global.handler.oauth.OAuth2LoginSuccessHandler;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private final LoginService loginService;
 	private final JwtService jwtService;
 	private final MemberRepository memberRepository;
@@ -35,6 +37,20 @@ public class SecurityConfig {
 	private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 	private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
 	private final CustomOAuth2UserService customOAuth2UserService;
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/swagger-ui/**","/webjars/**","/css/**","/js/**","/v2/api-docs","/v3/api-docs" ,"/configuration/ui",
+				"/swagger-resources/**", "/configuration/security",
+				"/swagger-ui.html", "/webjars/**","/swagger/**");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/swagger-ui/**","/oauth2/authorization/**").permitAll()
+				.anyRequest().authenticated();
+	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -52,7 +68,7 @@ public class SecurityConfig {
 				.authorizeRequests()
 
 				.antMatchers("/","/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**").permitAll()
-				.antMatchers("/callback/**","swagger-ui/**","/oauth2/authorization/**").permitAll() // 회원가입 접근 가능
+				.antMatchers("/callback/**","swagger-ui/index.html","/oauth2/authorization/**").permitAll() // 회원가입 접근 가능
 				.anyRequest().authenticated() // 위의 경로 이외에는 모두 인증된 사용자만 접근 가능
 				.and()
 				.oauth2Login()
