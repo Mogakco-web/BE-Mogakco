6234ee5b080fe216c5f3cc0de208e78eb10c023a
seonghoo1217 <seonghoo1217@naver.com>
2023-03-06 18:56:10 +0900

    #1 refactor : modify security authorization url

--- mogakco/src/main/java/project/mogakco/global/exception/TokenException.java
+++ mogakco/src/main/java/project/mogakco/global/exception/TokenException.java
@@ -3,7 +3,6 @@ package project.mogakco.global.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class TokenException extends RuntimeException{
 
 	public TokenException() {
