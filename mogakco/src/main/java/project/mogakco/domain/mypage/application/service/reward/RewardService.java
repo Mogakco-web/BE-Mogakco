86f30ad2a02f0ed47368b21dafd88e1fdefda8e8
seonghoo1217 <seonghoo1217@naver.com>
2023-04-13 19:16:28 +0900

    #37 fix : solved #39 issues used to DIP

--- mogakco/src/main/java/project/mogakco/domain/mypage/application/service/reward/RewardService.java
+++ mogakco/src/main/java/project/mogakco/domain/mypage/application/service/reward/RewardService.java
@@ -1,8 +1,7 @@
 package project.mogakco.domain.mypage.application.service.reward;
 
-import org.springframework.http.ResponseEntity;
 import project.mogakco.domain.member.entity.member.MemberSocial;
 
-public interface RewardService {
-	void initializeRewardService(String type, MemberSocial memberSocial);
+public abstract class RewardService {
+	public abstract void initializeRewardService(String type, MemberSocial memberSocial);
 }
