b0a8f92b35124c15d4a6d14dcb2b90943a4ac69c
seonghoo1217 <seonghoo1217@naver.com>
2023-04-18 14:58:53 +0900

    #37 feat : initialize reward value

--- mogakco/src/main/java/project/mogakco/domain/mypage/application/impl/reward/RewardServiceImpl.java
+++ mogakco/src/main/java/project/mogakco/domain/mypage/application/impl/reward/RewardServiceImpl.java
@@ -1,15 +1,24 @@
 package project.mogakco.domain.mypage.application.impl.reward;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import project.mogakco.domain.member.entity.member.MemberSocial;
 import project.mogakco.domain.mypage.application.service.reward.RewardService;
+import project.mogakco.domain.mypage.entity.DummyReward;
 import project.mogakco.domain.mypage.entity.Reward;
 import project.mogakco.domain.mypage.entity.RewardMemberSocial;
 import project.mogakco.domain.mypage.repo.RewardMemberSocialRepository;
 import project.mogakco.domain.mypage.repo.RewardRepository;
 import project.mogakco.domain.timer.application.service.TimerCheckService;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 
 @Service
@@ -23,6 +32,13 @@ public class RewardServiceImpl extends RewardService {
 
 	private final TimerCheckService timerCheckService;
 
+	private static List<DummyReward> dummyRewardList;
+
+	@PostConstruct
+	public void initializeReward(){
+		initializeRewardList();
+	}
+
 	@Override
 	public void initializeRewardService(String type,MemberSocial memberSocial) {
 		switch (type){
@@ -33,6 +49,8 @@ public class RewardServiceImpl extends RewardService {
 				timerInitialize(memberSocial);
 		}
 	}
+
+
 
 	@Transactional
 	public void timerInitialize(MemberSocial memberSocial) {
@@ -78,4 +96,23 @@ public class RewardServiceImpl extends RewardService {
 		);
 	}
 
+	@Override
+	public ResponseEntity<?> getListInfoRewardList() {
+		return new ResponseEntity<>(dummyRewardList, HttpStatus.OK);
+	}
+
+	private void initializeRewardList(){
+		LinkedList<String> name_list= (LinkedList<String>) Arrays.asList("뉴비","부서진 초시계(을)를 얻었다",
+				"초시계(750G) 획득!","존야의 모래시계","시간의 지배자");
+
+		LinkedList<String> des_list=(LinkedList<String>) Arrays.asList("모각코에 당도한 것을 환영하오 낯선이여",
+				"장비를 정지합니다","룬 : 완벽한 초시계","김동준의 가호가 함께합니다","따이무 쓰또쁘");
+
+		for (int i=0;i<name_list.size();i++){
+			dummyRewardList.add(DummyReward.builder()
+							.name(name_list.get(i))
+							.description(des_list.get(i))
+					.build());
+		}
+	}
 }
